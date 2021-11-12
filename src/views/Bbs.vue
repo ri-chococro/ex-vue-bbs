6d798b1300ca0cc1caf8d955ff08a52d1c895d09
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-12 12:23:02 +0900

    記事一覧を表示させる

--- src/views/Bbs.vue
+++ src/views/Bbs.vue
@@ -1,12 +1,24 @@
 <template>
-  <div class="bbs"></div>
+  <div class="bbs">
+    <div v-for="article of currentArticleList" v-bind:key="article">
+      <div>投稿者名：{{ article.name }}</div>
+      <div>投稿内容：</div>
+      <div>{{ article.content }}</div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class Bbs extends Vue {}
+export default class Bbs extends Vue {
+  currentArticleList = [];
+
+  created() {
+    this.currentArticleList = this["$store"].getters.getArticle;
+  }
+}
 </script>
 
 <style></style>

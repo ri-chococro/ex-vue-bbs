6da06eea0a76ebd3074f02cb82963e8586f02020
ri-chococro <riho.nishitarumi@rakus-partners.co.jp>
2021-11-12 11:42:40 +0900

    投稿内容を表すクラスを作成

---
+++ src/views/article.ts
@@ -0,0 +1,44 @@
+export class Article {
+  constructor(
+    // id
+    private _id: number,
+    // 投稿者名
+    private _name: string,
+    // 記事内容
+    private _content: string,
+    // コメント
+    private _commentList: Array<Comment>
+  ) {}
+
+  public get id(): number {
+    return this._id;
+  }
+
+  public set id(id: number) {
+    this._id = id;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set name(name: string) {
+    this._name = name;
+  }
+
+  public get content(): string {
+    return this._content;
+  }
+
+  public set content(content: string) {
+    this._content = content;
+  }
+
+  public get commentList(): Array<Comment> {
+    return this._commentList;
+  }
+
+  public set commentList(commentList: Array<Comment>) {
+    this._commentList = commentList;
+  }
+}
